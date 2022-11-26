7062ad3498bef5a453c3db996150f45cd85257c4
Gleb Zverev <s02190647@gse.cs.msu.ru>
2022-11-26 19:56:33 +0300

    hardhat

--- scripts/tree.js
+++ scripts/tree.js
@@ -3,8 +3,6 @@ const { config } = require("chai");
 const hre = require("hardhat");
 const { isConstructorDeclaration } = require("typescript");
 
-// const {deploy_book} = require("./book.js");
-
 async function deploy_book(){
     const Book = await hre.ethers.getContractFactory("MyBook"); 
     const book = await Book.deploy();
@@ -16,7 +14,6 @@ async function deploy_book(){
     return book;
 }
 
-
 async function valveDeploy(book_address, index){
     const Valve = await hre.ethers.getContractFactory("Valve")
     const valve = await Valve.deploy(book_address, index);
@@ -24,16 +21,37 @@ async function valveDeploy(book_address, index){
       console.log(
           `valve  deployed to ${valve.address}`
       );
-      valve.setPercents()
+    //   valve.setPercents()
     });
     return valve;
+}
+
+async function treeDeploy(book_address, amount){
+    valves = []
+    for (var i = 0; i < amount; i++){
+        var valve = await valveDeploy(book_address, i)
+        valves.push(valve);
+    }
+    return valves;
 }
 
 // async function valveDeploy()
 
 async function main(){
     // await valveDeploy(env["beneficiary"][0], 0)
+    const beneficicary = env["beneficiary"];
     const book = await deploy_book();
+    const valves = await treeDeploy(book.address, 6)
+    console.log(valves[1].address);
+
+    valves[1].setPercents();
+    await book.mint(valves[1].address, 0, 100, "0x").then((_, err) =>{
+        if (err) 
+          console.log(err);
+        else{
+          book.percents(0,0).then(console.log)
+        }
+      })
 }
 
 
